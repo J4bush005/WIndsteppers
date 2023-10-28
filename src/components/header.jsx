5ac4b7f480316411040770574562852f1932a490
j4bush005 <jtforbush429@gmail.com>
2023-10-27 23:59:51 -0500

    styled navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,38 +5,53 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
 import Logo from "../Photos/Updated Windsteppers-Logo.webp";
-import Cart from "../Photos/shopping-cart.webp"
+import Cart from "../Photos/shopping-cart.webp";
 
 function header() {
   return (
-    <Navbar expand="lg" className="bg-none">
-      <Container fluid>
-        <Navbar.Brand href="#home">
-          <img src={Logo} alt="" className=" align-top" />
-        </Navbar.Brand>
-        <div className="d-flex">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        
-        <Navbar.Collapse id="basic-navbar-nav" >
-          <Nav className="">
-            <NavDropdown title="About Us" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">
-                Our History
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Coaches</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Gallery</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#home">Athletes</Nav.Link>
-            <Nav.Link href="#home">Events/Calendar</Nav.Link>
-            <Nav.Link href="#home">Store</Nav.Link>
-            <Nav.Link href="#home" >
-                <img src={Cart} alt="" width="30" />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        </div>
-      </Container>
-    </Navbar>
+    <div id="nav-styles">
+      <Navbar expand="lg" className="bg-none">
+        <Container fluid>
+          <Navbar.Brand href="#home">
+            <img src={Logo} alt="" className=" align-top" />
+          </Navbar.Brand>
+          <div className="d-flex">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="text-light">
+                <NavDropdown
+                  title={<span className="text-light">About Us</span>}
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item href="#action/3.1">
+                    Our History
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Coaches
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Gallery
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#home" className="text-light">
+                  Athletes
+                </Nav.Link>
+                <Nav.Link href="#home" className="text-light">
+                  Events/Calendar
+                </Nav.Link>
+                <Nav.Link href="#home" className="text-light">
+                  Store
+                </Nav.Link>
+                <Nav.Link href="#home">
+                  <img src={Cart} alt="" width="30" />
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
