e06550f9cd0a75b5c762df4ae65287247f30d590
j4bush005 <jtforbush429@gmail.com>
2023-10-28 00:51:10 -0500

    animation in progress

--- src/components/open-image.jsx
+++ src/components/open-image.jsx
@@ -1,20 +1,22 @@
 import React, { useRef } from "react";
 import Header from ".//header";
-import { useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 function Open() {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["start start", "end start"],
   });
-  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "200%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["0%", "20%"]);
   return (
+    <motion.div style={{y:textY}} >
     <section id="open-section" className="page-section call-to-action d-flex">
       <Header />
       <h1 id="team-name" className="text-break">
         Windsteppers
-      </h1>
+     </h1>
     </section>
+     </motion.div>
   );
 }
 
--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -7,17 +7,18 @@ function Home() {
         target: ref,
         offset: ["start start", "end start"],
     });
-    const backgroundY = useTransform(scrollYProgress, [0,1], ["0%", "100%"]);
-    const textY = useTransform(scrollYProgress, [0,1], ["0%", "200%"]);
+    const backgroundY = useTransform(scrollYProgress, [0,1], ["0%", "-20%"]);
   return (
     <>
      <Open/>
-     <section>
+     <motion.div style={{y: backgroundY}}>
+     <section className="bg-white">
         <h1 id="test" className="d-flex justify-content-center">HELLO</h1>
         <h1 id="test" className="d-flex justify-content-center">HELLO</h1>
         <h1 id="test" className="d-flex justify-content-center">HELLO</h1>
         <h1 id="test" className="d-flex justify-content-center">HELLO</h1>
      </section>
+     </motion.div>
     </>
   );
 }
