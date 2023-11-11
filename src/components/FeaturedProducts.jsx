c9e950336a54047ee581091758f331e62bf4656f
j4bush005 <jtforbush429@gmail.com>
2023-11-11 15:04:56 -0600

    first featured product

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,24 +1,30 @@
-import React from 'react'
+import React from "react";
 import Shoes from "../Photos/Shoes.png";
 
 function FeaturedProducts() {
   return (
     <div>
-        <div id="product-background">
-            <div className="d-flex justify-content-evenly">
-            <div className="col-6">
-                <div className="display-1">Something catchy</div>
-                <div className="row">
-                    <div className="col-6">
-                        <div className="display-4">words</div>
-                    </div>
+      <div id="product-background">
+        <div className="d-flex justify-content-evenly">
+          <div id="catchy" className="col-6">
+            <div className="display-1">Something catchy</div>
+            <div className="row">
+              <div className="col-6">
+                <div className="display-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do
                 </div>
+              </div>
             </div>
-            <div className="col"><img src={Shoes} alt="" className="img-fluid" /></div>
-            </div>
+          </div>
+
+          <div className="col">
+            <img src={Shoes} alt="" className="img-fluid" />
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FeaturedProducts
+export default FeaturedProducts;
