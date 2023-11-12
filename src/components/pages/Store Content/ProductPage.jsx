09f0457a1a197c4d3bbb7573a2564d1d195311b7
j4bush005 <jtforbush429@gmail.com>
2023-11-11 21:25:49 -0600

    working on the product page

--- src/components/pages/Store Content/ProductPage.jsx
+++ src/components/pages/Store Content/ProductPage.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Store.css";
 function ProductPage() {
+    const [selectedImg,setSelectedImg] = useState(0)
+
+    const images = [
+        'https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+        'https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load',
+    ];
   return (
     <div className="container-fluid">
       <div className="row d-flex justify-content-evenly">
         <div className="col">
-          <div className="row">y</div>
-          <div className="row">ffghfhgjfhjfhjgfg</div>
+          <div className="row"><img src={images[0]} alt="" onClick={e=>setSelectedImg(0)} className="img-fluid"/></div>
+          <div className="row"><img src={images[1]} alt="" onClick={e=>setSelectedImg(1)} className="img-fluid"/></div>
         </div>
-        <div className="col">y</div>
+        <div className="col"><img src={images[selectedImg]} alt="" className="img-fluid" /></div>
         <div className="col">
           <div className="row">gfdgfd</div>
           <div className="row">gfdgfd</div>
