c407c82aa2b1b2858f9385396da48e23b6f9f591
Johnathon Forbush <jtforbush429@gmail.com>
2023-12-14 04:25:47 -0600

    removed extra code

--- src/components/pages/Home Content/home.jsx
+++ src/components/pages/Home Content/home.jsx
@@ -1,8 +1,6 @@
 import React, { useRef } from "react";
 import Open from "./open-image";
-import SmallOpen from "./Open-sm";
 import { motion, useScroll, useTransform } from "framer-motion";
-import Media from "react-media";
 import Content from "./homeContent";
 import "./Home.css";
 
@@ -14,7 +12,7 @@ function Home() {
     offset: ["start start", "end start"],
   });
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "-40%"]);
-  const backgroundYs = useTransform(scrollYProgress, [0, 1], ["0%", "-0npm%"]);
+
 
   return (
     <>
