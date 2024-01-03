259ba1d53682631a009d204cb44093ed6ea1ebd6
Johnathon Forbush <jtforbush429@gmail.com>
2024-01-02 20:21:11 -0600

    test

--- src/components/pages/Home Content/open-image.jsx
+++ src/components/pages/Home Content/open-image.jsx
@@ -7,14 +7,14 @@ function Open() {
     target: ref,
     offset: ["start start", "end start"],
   });
-  const moveUp = useTransform(scrollYProgress, [0, 1], ["0%", "20%"]);
+  const moveUp = useTransform(scrollYProgress, [0, 1], ["0%", "0%"]);
   const textY = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
   const moveHeader = useTransform(scrollYProgress, [0, 1], ["0%", "-20%"]);
   const moveScripture = useTransform(scrollYProgress, [0, 1], ["0%", "-100%"]);
   return (
     
     <motion.div
-      initial={{ y: 500 }}
+      initial={{ y: 0 }}
       whileInView={{ y: 0 }}
       transition={{ duration: 0.8 }}
       viewport={{ once: true }}
