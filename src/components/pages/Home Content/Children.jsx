de46d8daa722af69912bf91950950a7f842fd7e8
Johnathon Forbush <jtforbush429@gmail.com>
2024-01-11 17:19:07 -0600

    filling out paragrphs

--- src/components/pages/Home Content/Children.jsx
+++ src/components/pages/Home Content/Children.jsx
@@ -9,13 +9,18 @@ function Children() {
         <div className="row">
           <div className="col-md-4 col-sm-12 col-xs-12" id="">
             <div className=" fs-4 d-flex" id="wordss">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              At Windsteppers Track Club, we are committed to the holistic
+              development of youth athletes, catering to ages 4 through college
+              level. Our foundational focus lies with elementary, middle, and
+              high school athletes, ensuring a low coach-to-athlete ratio for
+              personalized attention. With a dedicated coaching staff in jumps,
+              sprints, racewalk, and throws, we prioritize the long-term growth
+              of each individual. Emphasizing versatility, our younger athletes,
+              up to middle school age, receive comprehensive training across
+              various events, particularly up to the 400 meters. As athletes
+              showcase their abilities through training and meets, we tailor
+              their development, allowing for specialization in shorter sprints,
+              longer events, or even racewalking for those inclined differently.
             </div>
           </div>
           <div className="col-md-8 col-sm-12 col-xs-12">
@@ -30,13 +35,18 @@ function Children() {
           </div>
           <div className="col-md-8 col-sm-12 col-xs-12">
             <div className=" fs-4 d-flex" id="wordss">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              At Windsteppers Track Club, we are committed to the holistic
+              development of youth athletes, catering to ages 4 through college
+              level. Our foundational focus lies with elementary, middle, and
+              high school athletes, ensuring a low coach-to-athlete ratio for
+              personalized attention. With a dedicated coaching staff in jumps,
+              sprints, racewalk, and throws, we prioritize the long-term growth
+              of each individual. Emphasizing versatility, our younger athletes,
+              up to middle school age, receive comprehensive training across
+              various events, particularly up to the 400 meters. As athletes
+              showcase their abilities through training and meets, we tailor
+              their development, allowing for specialization in shorter sprints,
+              longer events, or even racewalking for those inclined differently.
             </div>
           </div>
         </div>
