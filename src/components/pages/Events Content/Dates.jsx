c793efb3109238b0e52b840f6124c10a219d1e8c
Johnathon Forbush <jtforbush429@gmail.com>
2024-01-19 23:22:09 -0600

    adding the events page

---
+++ src/components/pages/Events Content/Dates.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Dates() {
+  return (
+    <>
+    <div className="container">
+        <div className="d-flex justify-content-center">
+          {" "}
+          <h1 className="display-2" id="championsss">
+            Important Dates:
+          </h1>
+        </div>
+        <div className="d-flex justify-content-center">
+          {" "}
+          <h1 className="display-2" id="">
+            Come Back Soon to See the Important Dates and Locations For the 2024 Season!
+          </h1>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Dates
--- src/components/pages/Events Content/Events.jsx
+++ src/components/pages/Events Content/Events.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from "../../header";
+import Footer from '../../footer';
+import Dates from './Dates';
 function Events() {
   return (
     <div>
     <Header/>
+    <Dates/>
+    <Footer/>
     </div>
   )
 }
