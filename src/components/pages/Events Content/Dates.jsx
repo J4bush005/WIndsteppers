4bfa98ca6eb1a6304444a8ad54e37fe3a28cabf8
Johnathon Forbush <jtforbush429@gmail.com>
2024-04-15 08:34:50 -0500

    adding a calendar

--- src/components/pages/Events Content/Dates.jsx
+++ src/components/pages/Events Content/Dates.jsx
@@ -7,14 +7,27 @@ function Dates() {
         <div className="d-flex justify-content-center">
           {" "}
           <h1 className="display-2" id="championsss">
-            Important Dates:
+            Important Dates and Events:
           </h1>
         </div>
         <div className="d-flex justify-content-center">
           {" "}
-          <h1 className="display-2" id="">
-            Come Back Soon to See the Important Dates and Locations For the 2024 Season!
-          </h1>
+          <h3 className="fs-3" >
+            <ul id="list">
+              <li>April 20, 2024 – Girard Warriors Meet @ Girard High/Middle</li>
+              <li>May 4, 2024 - Team United Stars Meet @ Seaman HS, Topeka, KS</li>
+              <li>May 11, 2024- Runstars Meet @ Leavenworth HS, Leavenworth, KS</li>
+              <li> May 18, 2024 - Windstepper Meet – location TBD</li>
+              <li> May 27, 2024 - Team Believe Meet @ Central High School</li>
+              <li>June 1, 2004 - AAU District Qualifier in Kansas</li>
+              <li>June 7-8, 2024 - AAU District Qualifier in Missouri</li>
+              <li>June 15, 2024 – Wichita Athletics Meet @ Friends University, OP KS</li>
+              <li>June 19-22, 2024 - AAU Regionals @ Bentonville West HS, Centerton, AR</li>
+              <li>July 13, 2024 – Pony Express Meet – location TBD</li>
+              <li> July 27 – August 3, 2024 - AAU Nationals @ North Carolina A&T University in Greensboro, NC</li>
+              <li>August 18, 2024 - Windsteppers Banquet</li>
+            </ul>
+          </h3>
     </div>
     </div>
     </>
--- src/components/pages/Events Content/Events.jsx
+++ src/components/pages/Events Content/Events.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from "../../header";
 import Footer from '../../footer';
 import Dates from './Dates';
+import "./Events.css"
 function Events() {
   return (
     <div>
